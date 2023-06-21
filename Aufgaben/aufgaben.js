fabd219b0a84b43e48452aee48df4fe131701e42
Zimmermann Levin (LEZ) <levin.zimmermann@suva.ch>
2023-06-21 16:12:23 +0200

    background update + css update

--- Aufgaben/aufgaben.js
+++ Aufgaben/aufgaben.js
@@ -1,17 +1,29 @@
-//  JS
-//  Aufgabe 4
-//  Geben Sie "Hello World" in der Konsole aus.
+/*
+  Aufgabe 4
+  Gib "Hello World" in der Konsole aus.
+  Wir werden dir zeigen, wie du in die Konsole gelangst.
+*/
 
 
-//  Aufgabe 5
-//  Überschreibe mit hilfe von JavaScript die Überschrift aus Aufgabe 1 mit "Meine Oberstiften sind so cool!"
+
+/*
+  Aufgabe 5
+  Überschreibe mit hilfe von JavaScript die Überschrift aus Aufgabe 1 mit "Meine Oberstiften sind so cool!"
+*/
 
 
-//  Aufgabe 6
-//  Schreibe eine Funktion, die in dem div mit der id = myBox, "Hello World" ausgibt.
+
+/*
+  Aufgabe 6
+  Schreibe eine Funktion, die in dem div mit der id = myBox, "Hello World" ausgibt.
+*/
 
 
-//  Aufgabe 7
-//  Schreibe eine Funktion, die überprüft ob der Button mit der id = myButton geklickt wird oder nicht. Wenn dieser geklickt wird, soll eine Zahl aufsteigen. Für jeden klick + 1.
 
+/*
+  Aufgabe 7
+  Schreibe eine Funktion, die überprüft ob der Button mit der id = myButton geklickt wird oder nicht. 
+  Wenn dieser geklickt wird, soll eine Zahl aufsteigen. 
+  Für jeden klick + 1.
+*/
 
